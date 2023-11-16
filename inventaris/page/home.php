98882cc06de0038e1adcbb9e4c844fd8a427590d
PermanaKienan <150992086+PermanaKienan@users.noreply.github.com>
2023-11-16 12:56:38 +0700

    Add files via upload

---
+++ inventaris/page/home.php
@@ -0,0 +1,6 @@
+<div class="jumbotron">
+        <h1>Selamat Datang <?= $user ?> !</h1>
+        <p>Selamat Datang di INVENTARIS.</p>
+        <p>To see the difference between static and fixed top navbars, just scroll.</p>
+        
+      </div>
